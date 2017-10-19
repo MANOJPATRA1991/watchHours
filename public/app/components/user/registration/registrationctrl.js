e203a07ab62c679f73d0f1cc487aadbc4e282c9c
MANOJ PATRA <patra.manoj0@gmail.com>
2017-10-19 20:39:01 +0530

    Fix issue with registration

--- public/app/components/user/registration/registrationctrl.js
+++ public/app/components/user/registration/registrationctrl.js
@@ -89,6 +89,9 @@ angular.module('watchHoursApp')
                     { type: 'danger', msg: "Passwords don't match" }
                 )];
             }
+            $rootScope.$on('login:Successful', function(){
+                $state.go("app");
+            });
             $rootScope.$on('login:Unsuccessful', function(){
                 $scope.alerts = [(
                     { type: 'danger', msg: AuthFactory.Error() }
